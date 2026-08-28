dc0c24fd398e3dfa790a9859d53a71d53dcd47b9
SamDelorean <the_crown_mx@yahoo.com>
2026-08-28 14:28:28 -0600

    os3k: declare ClipboardClear ABI

---
+++ os3k/clipboard_clear.h
@@ -0,0 +1,7 @@
+#ifndef _OS3K_CLIPBOARD_CLEAR_H_
+#define _OS3K_CLIPBOARD_CLEAR_H_
+
+/* Clear clipboard logical/allocation state; backing bytes are not securely erased. */
+void ClipboardClear(void);
+
+#endif
