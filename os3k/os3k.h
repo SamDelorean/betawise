2ca5ffc8bef382932bb6d46ad0d01f155b5305d7
SamDelorean <the_crown_mx@yahoo.com>
2026-08-28 12:00:06 -0600

    os3k: correct A25C service dispatcher ABI

--- os3k/os3k.h
+++ os3k/os3k.h
@@ -231,8 +231,8 @@ uint8_t AppletFindById(uint16_t id);
 int AppletGetName(uint8_t index, char* name);
 int AppletSendMessage(uint8_t index, Message_e message, uint32_t param, uint32_t* status);
 
-// mask=0x8 to process special key. mask=4,2,1 unknown.
-void SYS_A25C(uint8_t mask, KeyMod_e key);
+// action_mask bits are dispatched independently; bit 3 handles special keys.
+int32_t SYS_A25C(uint32_t action_mask, KeyMod_e key);
 
 uint32_t CallSysInt(uint32_t unused_zero, SysInt_e info, void* output);
 
