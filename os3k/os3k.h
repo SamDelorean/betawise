00d0274f8246d205f76ebafb6aa444a877ac0dc5
SamDelorean <the_crown_mx@yahoo.com>
2026-08-28 11:26:11 -0600

    os3k: declare A24C master password verifier

--- os3k/os3k.h
+++ os3k/os3k.h
@@ -231,6 +231,9 @@ uint8_t AppletFindById(uint16_t id);
 int AppletGetName(uint8_t index, char* name);
 int AppletSendMessage(uint8_t index, Message_e message, uint32_t param, uint32_t* status);
 
+// Non-interactive master-password comparison. Only the low byte is contractual.
+uint8_t SYS_A24C(const char *password);
+
 // mask=0x8 to process special key. mask=4,2,1 unknown.
 void SYS_A25C(uint8_t mask, KeyMod_e key);
 
