7f69a2628ec188b331c3753b2f2b9aca596f1751
SamDelorean <the_crown_mx@yahoo.com>
2026-08-28 21:12:24 -0600

    os3k: expose A248 password protection getter

--- os3k/os3k.h
+++ os3k/os3k.h
@@ -231,6 +231,9 @@ uint8_t AppletFindById(uint16_t id);
 int AppletGetName(uint8_t index, char* name);
 int AppletSendMessage(uint8_t index, Message_e message, uint32_t param, uint32_t* status);
 
+// Raw file-password-protection state getter. Only the low byte is contractual.
+uint8_t SYS_A248(void);
+
 // mask=0x8 to process special key. mask=4,2,1 unknown.
 void SYS_A25C(uint8_t mask, KeyMod_e key);
 
